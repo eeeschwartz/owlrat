d58996e52d577a5bc2e17555c756e130644142ae
Erik Schwartz <erik@tablexi.com>
2013-05-31 09:27:28 -0500

    Add jquery animation library

---
+++ javascripts/main.js
@@ -0,0 +1,23 @@
+$(function() {
+  $('.owl').tween({
+    left:{
+      start: 0,
+      stop: 300,
+      time: 0,
+      units: 'px',
+      duration: 2,
+      effect:'easeInOut'
+    }
+  });
+  $('.owl').tween({
+    left:{
+      start: 300,
+      stop: 0,
+      time: 2,
+      units: 'px',
+      duration: 2,
+      effect:'easeInOut'
+    }
+  });
+  $.play();
+});
